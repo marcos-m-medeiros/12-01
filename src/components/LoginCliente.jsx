7c6f37662b970b03753a43127c742bb367a1dd02
Marcos Medeiros <marcosbourdon@hotmail.com>
2024-01-12 09:27:24 -0300

    fodase

--- src/components/LoginCliente.jsx
+++ src/components/LoginCliente.jsx
@@ -1,7 +1,9 @@
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import React from "react";
 import { auth } from "../../firebaseconfig";
-import { View } from "react-native-web";
+import { View, Image, StyleSheet } from "react-native-web";
+import { Button } from "react-native-paper";
+import { ImageList } from "../../assets/imageList";
 
 export default function LoginCliente() {
   function handleGoogleAuthSignIn() {
@@ -13,9 +15,25 @@ export default function LoginCliente() {
 
   return (
     <View>
-      <button onClick={handleGoogleAuthSignIn}>
-        RODAR AUTENTICAÇÃO GOOGLE
-      </button>
+      <Image style={styles.loginImage} source={ImageList.loginImage} />
+      <Button
+        style={styles.loginButton}
+        label=""
+        onClick={handleGoogleAuthSignIn}
+      >
+        Logar
+      </Button>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  loginImage: {
+    height: "12rem",
+    width: "100%",
+  },
+  loginButton: {
+    alignItems: "center",
+    backgroundColor: "",
+  },
+});
